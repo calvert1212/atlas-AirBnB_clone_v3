8e9a04ecd5be4696f13d94f4574984c182ba9a9f
calvert1212 <calvert1212@gmail.com>
2024-03-21 01:00:54 +0000

    Fixing app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,31 +6,36 @@ and a custom error handler for 404 errors.
 """
 
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, render_template
 from models import storage
 import os
-from os import getenv
 from flask_cors import CORS
 
 """Creates a Flask application instance"""
 app = Flask(__name__)
 
+"""Configure CORS to allow requests from any origin."""
+"""Necessary for client-side web applications to interact with the API."""
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 """Registers the blueprint for the API views"""
 app.register_blueprint(app_views)
 
-"""Configure CORS to allow requests from any origin."""
-"""Necessary for client-side web applications to interact with the API."""
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
-def teardown_appcontext(exception=None):
+def teardown_db(exception):
     """Closes storage when the application context is torn down."""
     storage.close()
 
+
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(exception):
     """Return a JSON response for 404 errors."""
-    return (jsonify({"error": "Not found"}), 404)
+    code = exception.__str__().split()[0]
+    description = "Not found"
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
 
 if __name__ == "__main__":
     """Use environment variables for host and port, with defaults if not set."""
