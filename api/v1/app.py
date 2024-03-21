046cab2538fe52ebe5561009e82f30ada463b279
calvert1212 <calvert1212@gmail.com>
2024-03-21 01:57:23 +0000

    Trying to fix app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,31 +6,34 @@ and a custom error handler for 404 errors.
 """
 
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, render_template
 from models import storage
-import os
+from flask import Flask, jsonify, make_response, render_template
 from flask_cors import CORS
+import os
 
-"""Creates a Flask application instance"""
+"""Creates Flask instance"""
 app = Flask(__name__)
+"""Configures CORS to allow requests, needed"""
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
-"""Configure CORS to allow requests from any origin."""
-"""Necessary for client-side web applications to interact with the API."""
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+"""Sets up Flask environment variables"""
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
 
-"""Registers the blueprint for the API views"""
+"""Registers Blueprint for app_views"""
 app.register_blueprint(app_views)
 
 
+"""Tears down Flask"""
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Closes storage when the application context is torn down."""
+    """Tears down and closes Flask"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(exception):
-    """Return a JSON response for 404 errors."""
+def handle_404(exception):
+    """Handler for 404 error (Not Found)"""
     code = exception.__str__().split()[0]
     description = "Not found"
     message = {'error': description}
@@ -38,8 +41,6 @@ def page_not_found(exception):
 
 
 if __name__ == "__main__":
-    """Use environment variables for host and port, with defaults if not set."""
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    """Run the Flask server with the specified host and port, using threaded mode for better performance."""
-    app.run(host=host, port=port, threaded=True)
+    """MAIN Flask App"""
+    # start Flask app
+    app.run(host=host, port=port)
