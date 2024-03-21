ee6f61a4ce5fcacfe9ca26d40e4a995844c6c37e
calvert1212 <calvert1212@gmail.com>
2024-03-21 01:04:49 +0000

    added cities import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """View Blueprint"""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
@@ -10,3 +10,4 @@ from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places import *
+from api.v1.views.cities import *
